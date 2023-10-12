2624e251bdf69f915d639ccb7adc39708256bd13
sw-itok <itok@solutionware.jp>
2023-10-13 00:51:19 +0900

    configに寄せる

--- server/src/config/db/sequelize.config.ts
+++ server/src/config/db/sequelize.config.ts
@@ -1,4 +1,6 @@
 import { Sequelize } from 'sequelize';
+import { Users, initUsersModel } from '../../models/Users';
+import { Tasks, initTasksModel } from '../../models/Tasks';
 
 export const sequelize = new Sequelize('postgres', 'postgres', 'passw0rd', {
   host: '127.0.0.1',
@@ -6,3 +8,23 @@ export const sequelize = new Sequelize('postgres', 'postgres', 'passw0rd', {
   dialect: 'postgres',
   logging: false
 });
+
+const setupAssociations = () => {
+  Users.hasMany(Tasks, { foreignKey: 'user_id' });
+  Tasks.belongsTo(Users, { foreignKey: 'user_id' });
+};
+
+initUsersModel(sequelize);
+initTasksModel(sequelize);
+setupAssociations();
+
+// const recreateForceTable = () => {
+//   Tasks.sync({ force: true })
+//     .then(() => {
+//       console.log('Student table has been successfully recreated.');
+//     })
+//     .catch(error => {
+//       console.error('Error recreating Student table:', error);
+//     });
+// };
+// recreateForceTable();
