148e5874b89c9456e3e9b0ac694315813458fbc6
sw-itok <kira_neymar@icloud.com>
2023-12-03 02:16:48 +0900

    'React' is declared but its value is never read

--- client/src/pages/TaskDetailPage.tsx
+++ client/src/pages/TaskDetailPage.tsx
@@ -1,5 +1,4 @@
 import { Box } from '@chakra-ui/react';
-import React from 'react';
 import { Header } from '../components/Header';
 import { TaskDetail } from '../features/tasks/components/TaskDetail/TaskDetail';
 
